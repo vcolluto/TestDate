2bdb2eb363eb73a31acec9209ebacda6f1a81c2b
Enzo Colluto <vcolluto>
2022-10-11 12:11:00 +0200

    aggiunto metodo per confrontare date, per calcolare differenza tra date e gestione dell'eccezione data non valida

--- TestDate/src/it/corsojava/Main.java
+++ TestDate/src/it/corsojava/Main.java
@@ -1,11 +1,14 @@
 package it.corsojava;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
 	public static void main(String[] args) {
 		LocalDate d1,d2;
+		
 		
 		// dichiarazione di una data generica	
 		d1=LocalDate.of(2022, 10, 10);	//creo una LocalDate del 10/10/2022
@@ -20,6 +23,26 @@ public class Main {
 		//  yyyy: anno con due cifre
 		DateTimeFormatter df=DateTimeFormatter.ofPattern("dd/MM/yyyy"); 
 		System.out.println("La data di oggi è: "+d2.format(df));
+		
+		
+		//verifica se una data è successiva ad un'altra
+		if (d2.isAfter(d1))
+			System.out.println("la seconda data è successiva alla prima");
+		else if (d2.isBefore(d1))
+			System.out.println("la seconda data è precedente alla prima");
+		
+		
+		//calcolo della differenza in giorni tra due date
+		long g=ChronoUnit.DAYS.between(d1, d2);
+		System.out.println("Sono passati "+g+ " giorni");
+		
+		
+		//intercettare l'eccezione di data non valida		
+		try {				
+			d1=LocalDate.of(2021, 2, 31);	//creo una LocalDate del 31/2/2021 (NON VALIDA!)
+		} catch (DateTimeException e) {
+			System.out.println("Data non valida: "+e.getMessage());			
+		}
 		
 	}
 }
